d4cfdc66ccd61744507ab198e1d9f1e5577b3bcd
shaulbarlev <shaulberlev1@gmail.com>
2026-03-08 09:31:27 +0200

    Update base URL in Vite configuration to use the full domain for deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/sbl-projects/',
+  base: 'https://shaulb.com/sbl-projects/',
   plugins: [
     react(),
     tailwindcss(),
