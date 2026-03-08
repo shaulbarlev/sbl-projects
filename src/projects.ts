66fb78a97202fcdfe69eb12208c22c7a1bd73cfa
shaulbarlev <shaulberlev1@gmail.com>
2026-03-08 18:09:01 +0200

    Remove IMG_9666.MP4 video file and update its reference in projects.ts to IMG_9666-1.jpg for consistency in media assets.

--- src/projects.ts
+++ src/projects.ts
@@ -259,7 +259,7 @@ In terms of electronics, it was pretty straightforward: I used a buck converter,
       { kind: 'file', src: '/midi-controller/IMG_9666.mp4', title: 'MIDI controller', thumbnail: '/midi-controller/IMG_9666.jpg' },
     ],
     images: [
-      { src: '/midi-controller/IMG_9666.jpg', alt: 'MIDI controller' },
+      { src: '/midi-controller/IMG_9666-1.jpg', alt: 'MIDI controller' },
       { src: '/midi-controller/IMG_9670-0001.png', alt: 'MIDI controller' },
     ],
     description:
